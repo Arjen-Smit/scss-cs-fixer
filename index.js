832579f7b15faf0681ff52d7d1d784271d15b2c9
Arjen Smit <arjen@alnitak.nl>
2016-05-14 23:02:13 +0200

    add seperate files for different violations, and added glob

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var glob = require('glob');
 var config = require('./src/config.js')();
 var list = config.getEnabledOperatorList();
 
-console.log(list);
-
-console.log(config.getOperatorSettings('StringQuotes'));
+glob("fixture/scss/**/*.scss", function (err, files) {
+    console.log(files, err);
+})
